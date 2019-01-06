2f347a3b8d441b1c4a47830490d3d08d1a4a9ecd
MichSalv <misax.117@gmail.com>
2019-01-06 21:38:57 +0100

    Aggiunte funzione per creare grafo con e senza cicli

--- hasCycleUF.py
+++ hasCycleUF.py
@@ -1,52 +1,81 @@
 from unionfind import quickUnion
 from graph.Graph_AdjacencyList import *
 import random
+from time import time
 
-def hasCycleUf():
+def hasCycleUf(graph):
     uf = quickUnion.QuickUnionBalanced()
-    uf.makeSet(2)
-    uf.makeSet(3)
-    uf.union(uf.nodes[0],uf.nodes[1])
+
+    # per ogni nodo una union find
+    for node in graph.getNodes():
+        uf.makeSet(node.id)
+
     uf.print()
 
+    for edge in graph.getEdges():
+        print("Head root of node {}: {} Tail root of node {}: {}".format(edge.tail, uf.find(uf.nodes[edge.tail]) ,edge.head,uf.find(uf.nodes[edge.head])))  # uf.nodes[i] per come è creata la find serve
+
+        if uf.find(uf.nodes[edge.tail]) == uf.find(uf.nodes[edge.head]):        # uf.nodes[i] per come è creata la find serve
+            print("IF")
+            return 1
+        else:
+            uf.union(uf.nodes[edge.tail],uf.nodes[edge.head])           # TODO RICONTROLLARE SE E' CORRETTO L'INSERIMENTO DEGLI ARCHI (testa e coda sono invertiti!?)
+
+    uf.print()
+    return 0                                ## Sembra funzionare
 
 
-
-
-def createGraph():
+def createGraph(cycle = None):
     graph = GraphAdjacencyList()
-    graph.print()
-
-    nodi = []
-    for i in range(10):
+    # numNodes = random.randint(0,100)
+    numNodes = 10
+    nodes = []
+    for i in range(numNodes):
         node = graph.addNode(i)
-        print("Node inserted: ", node)
-        nodi.append(node)
+        nodes.append(node)
+        # print("Node(id) inserted: ", node.id)
+    i = 0
+    while(i<len(nodes) and i<numNodes-1):                    ## SERVIREBBE UN ITERATOR PER EVITARE DI COLLEGARE PIU' ARCHI ALLO STESSO NODO ?? oppure while con condizione i<len
 
-
-    graph.print()
-    # num = random.randint(0, len(nodi))
-    num = 4
-
-    print("num: {}".format(num))
-
-    # CONNETTO num NODI, non tutti --> creo un ciclo? 
-    for i in range(num):
-        for j in range(num):
-            node_1 = nodi[i]
-            node_2 = nodi[j]
+            node_1 = nodes[i]
+            node_2 = nodes[i+1]
             if node_1 != node_2:
-                print("node_1: {};  node_2: {}; \nAdiacenti: {}".format(node_1.id, node_2.id, graph.isAdj(node_1.id, node_2.id)))
-                graph.insertEdge(node_1.id, node_2.id)
-                print("Aggiunto arco tra {} e {}\nAdiacenti: {}".format(node_1.id, node_2.id, graph.isAdj(node_1.id, node_2.id)))
+                # print("Node_1: {}\nNode_2: {}\n{}".format(node_1.id, node_2.id, graph.getAdj(node_1.id)))
+                if graph.getAdj(node_1.id) == []:
+                    # print("Nodo {} non ha adiacenti".format(node_1.id))
+                    graph.insertEdge(node_1.id, node_2.id)
+                    # print("Inserito arco\n-----")
+            i += 1
+    if cycle:
+        numCycles = 1
 
-
-            print("------")
-
-
+        while numCycles>0:
+            nodeH_pos = random.randint(0,len(nodes)-1)
+            print(nodeH_pos)
+            nodeH = nodes[nodeH_pos]
+            print(nodeH)
+            nodeT_pos = random.randint(0, len(nodes) - 1)
+            print(nodeT_pos)
+            nodeT = nodes[nodeT_pos]
+            print(nodeT)
+            print("Head {} Tail {}".format(nodeH, nodeT))
+            if (nodeH == nodeT) or graph.isAdj(nodeT.id, nodeH.id):
+                print("Già adiacenti o uguali")
+                pass
+            else:
+                graph.insertEdge(nodeT.id, nodeH.id)
+                numCycles-=1
 
     graph.print()
+    return graph
 
 
 if __name__ == "__main__":
-    createGraph()
+    g = createGraph()
+    if hasCycleUf(g):
+        print("Graph has cycles")
+    else:
+        print("Graph hasn't cycles")
+
+
+        # ERRORE: in caso di grafi senza cicli stampa solo i primi due elementi della singola union find rimanente 
